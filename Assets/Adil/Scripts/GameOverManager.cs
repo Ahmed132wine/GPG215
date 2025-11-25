3939f4c13e3b725fc1fc3c601ff4103cd521db3a
Adil zain <adilzain009@gmail.com>
2025-11-26 02:51:37 +0400

    added game over

---
+++ Assets/Adil/Scripts/GameOverManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOverManager : MonoBehaviour
+{
+    public static GameOverManager instance;
+
+    [Header("UI Reference")]
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private TextMeshProUGUI finalScoreText;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+        if (gameOverPanel != null) gameOverPanel.SetActive(false);
+    }
+
+    public void TriggerGameOver()
+    {
+        if (gameOverPanel != null)
+        {
+            gameObject.SetActive(true);
+
+            if (ScoreManager.instance != null)
+            {
+                finalScoreText.text = ScoreManager.instance.scoreText.text;
+            }
+        }
+
+        Time.timeScale = 0f;
+        Debug.Log("Game Over");
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+}
